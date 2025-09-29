9768d7959834fc46536c6e65d6c4dddf2c94cd80
Rafael Lezcano <126247273+RafaLezcano-telco@users.noreply.github.com>
2025-09-29 17:06:50 -0300

    prueba render

--- frontend/public/app.js
+++ frontend/public/app.js
@@ -1,7 +1,7 @@
 const API_URL =  "https://todo-api-latest-gpya.onrender.com/api"
 async function cargarTareas() {
     try {
-        console.log("prueba")
+        console.log("prueba RENDER")
         const respuesta = await fetch(`${API_URL}/todos`);
         const tareas = await respuesta.json();
 
